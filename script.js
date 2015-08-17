69d7c79ae3502aec3bb157b6f1c6a1900ab854dd
aaron <harms280@gmail.com>
2015-08-17 13:18:25 -0700

    finished random color board

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Your JS goes here
 window.onload = function() {
-	createBoard();
+	createColorBoard();
 };
 
 document.body.style.height = "1000px";
@@ -8,26 +8,39 @@ var body = document.querySelector("body");
 
 var select = document.getElementsByTagName("div");
 
-function createBoard() {
+// function createBoard() {
+// 	for(var y = 1; y < 10 ; y++) {
+// 		for (var x = 0; x < 9; x++) {
+// 			var tile = document.createElement("div");
+// 			tile.style.width = "11.1%";
+// 			tile.style.float = "left";
+// 			tile.style.height = "11.1%";
+// 			if(y % 2 === 0) {
+// 				if(x % 2 === 0) {
+// 					tile.style.background = "red";
+// 				} else {
+// 					tile.style.background = "black";
+// 				}
+// 			} else {
+// 				if(x % 2 === 0) {
+// 					tile.style.background = "black";
+// 				} else {
+// 					tile.style.background = "red";
+// 				}
+// 			}
+// 			body.appendChild(tile);
+// 		}
+// 	}
+// }
+
+function createColorBoard () {
 	for(var y = 1; y < 10 ; y++) {
 		for (var x = 0; x < 9; x++) {
 			var tile = document.createElement("div");
 			tile.style.width = "11.1%";
 			tile.style.float = "left";
 			tile.style.height = "11.1%";
-			if(y % 2 === 0) {
-				if(x % 2 === 0) {
-					tile.style.background = "red";
-				} else {
-					tile.style.background = "black";
-				}
-			} else {
-				if(x % 2 === 0) {
-					tile.style.background = "black";
-				} else {
-					tile.style.background = "red";
-				}
-			}
+			tile.style.backgroundColor = "rgb("+Math.floor((Math.random()*255))+","+Math.floor((Math.random()*255))+","+Math.floor((Math.random()*255))+")";
 			body.appendChild(tile);
 		}
 	}
