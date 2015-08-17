773b41c8e05d08319e58ea90837bfcbb702ddcd0
aaron <harms280@gmail.com>
2015-08-17 15:15:16 -0700

    added gradient

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ window.onload = function() {
 document.body.style.height = "1000px";
 var body = document.querySelector("body");
 
+var filter = document.createElement("div");
+filter.style.width = "100%";
+filter.style.height = "1010px";
+filter.style.float = "left";
+filter.style.zIndex = "10";
+filter.style.position = "absolute";
+filter.style.top = "0";
+filter.style.left = "0";
+filter.style.background = "-webkit-linear-gradient(top, rgba(147,206,222,1) 0%, rgba(117,189,209,0.91) 41%, rgba(73,165,191,0.78) 100%)";
+
+body.appendChild(filter);
+
+
+
 var select = document.getElementsByTagName("div");
+
+
+
+
 
 function createBoard() {
 	for(var y = 1; y < 10 ; y++) {
